eb1e64e8d01dded1c0cc4160e3da3849416f5017
Plushken <96660351+Plushken@users.noreply.github.com>
2023-03-07 07:36:14 +0000

    add files and adding files and data entry checks

---
+++ src/interface.h
@@ -0,0 +1,15 @@
+//
+// created by ravinsky rodion
+//
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+// calc_day_before function calculates and return the day before
+// and calc_day_after function calculates and return the day after
+// implementation is in server.c file
+
+
+
+
+#endif
---
+++ src/server.c
@@ -0,0 +1,12 @@
+//
+//  created by ravinsky rodion
+//
+
+//  implementation of interface.h functions
+
+#include "interface.h"
+#include <ctype.h>
+#include <stdio.h>
+
+
+
